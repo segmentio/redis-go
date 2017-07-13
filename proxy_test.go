d12d7eb2b9d6667e4c30ac8c055f54772d10c599
Gerhard Esterhuizen <gerhard@segment.com>
2017-07-13 11:59:53 -0700

    Modularise the tests so that we can use the same code in the RED command in test mode.

--- proxy_test.go
+++ proxy_test.go
@@ -18,7 +18,7 @@ func TestReverseProxy(t *testing.T) {
 	redistest.TestClient(t, func() (redistest.Client, func(), error) {
 		transport := &redis.Transport{}
 
-		serverList, _ := makeServerList()
+		serverList, _, _ := makeServerList()
 
 		_, serverURL := newServer(&redis.ReverseProxy{
 			Transport: transport,
@@ -38,7 +38,7 @@ func TestReverseProxy(t *testing.T) {
 func TestReverseProxyHash(t *testing.T) {
 	transport := &redis.Transport{}
 
-	full, onedowns := makeServerList()
+	full, broken, onedowns := makeServerList()
 
 	proxy := &redis.ReverseProxy{
 		Transport: transport,
@@ -53,53 +53,46 @@ func TestReverseProxyHash(t *testing.T) {
 	// full backend - write n keys
 	n := 160
 	keyTempl := "redis-go.test.rphash.%d"
-	waiter := &sync.WaitGroup{}
-	for i := 0; i < n; i++ {
-		key := fmt.Sprintf(keyTempl, i)
-		val := "1"
-		waiter.Add(1)
-		go func(key, val string) {
-			defer waiter.Done()
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-			defer cancel()
-			if err := client.Exec(ctx, "SET", key, val); err != nil {
-				t.Error(err)
-				return
-			}
-		}(key, val)
-	}
-	waiter.Wait()
 
-	// full backend - read n back
-	numHits, numMisses, err := measHitMiss(t, client, n, keyTempl)
-	if err != nil {
-		t.Error(err)
+	numSuccess, numFailure, errs := writeBlock(client, n, keyTempl)
+	assert.Equal(t, n, numSuccess, "All writes succeeded")
+	assert.Equal(t, 0, numFailure, "No writes failed")
+	if errs != nil {
+		t.Error(errs)
 		return
 	}
-	t.Logf("full backend n read: numHits = %d, numMisses = %d", numHits, numMisses)
+
+	timeout := 30 * time.Second
+
+	// full backend - read n back
+	numHits, numMisses, err := measHitMiss(client, n, keyTempl, timeout)
+	t.Logf("full backend n read: numHits = %d, numMisses = %d, err = %v", numHits, numMisses, err)
+	assert.Nil(t, err, "Full backend - no errors")
 	assert.Equal(t, n, numHits, "Full backend - all hit")
 	assert.Equal(t, 0, numMisses, "Full backend - none missed")
 
 	// full backend - read n+1 back
-	numHits, numMisses, err = measHitMiss(t, client, n+1, keyTempl)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	t.Logf("full backend n+1 read: numHits = %d, numMisses = %d", numHits, numMisses)
+	numHits, numMisses, err = measHitMiss(client, n+1, keyTempl, timeout)
+	t.Logf("full backend n+1 read: numHits = %d, numMisses = %d, err = %v", numHits, numMisses, err)
+	assert.Nil(t, err, "Extra read - no errors")
 	assert.Equal(t, n, numHits, "Extra read - all hit")
 	assert.Equal(t, 1, numMisses, "Extra read - one (additional) missed")
+
+	// malfunctioning backend - read fails
+	proxy.Registry = broken
+	numHits, numMisses, err = measHitMiss(client, n+1, keyTempl, 2*time.Second)
+	t.Logf("broken backend n read: numHits = %d, numMisses = %d, err = %v", numHits, numMisses, err)
+	assert.NotNil(t, err, "Broken backend - errors")
+	assert.Equal(t, 0, numHits, "Broken backend - none hit")
+	assert.Equal(t, 0, numMisses, "Broken backend - none missed")
 
 	// single backend dropped (all combinations) - read n back
 	accHits, accMisses := 0, 0
 	for i := 0; i < len(onedowns); i++ {
 		proxy.Registry = onedowns[i]
-		numHits, numMisses, err = measHitMiss(t, client, n, keyTempl)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		t.Logf("single backend dropped (%d): numHits = %d, numMisses = %d, %d%% miss rate", i, numHits, numMisses, 100*numMisses/n)
+		numHits, numMisses, err = measHitMiss(client, n, keyTempl, timeout)
+		t.Logf("single backend dropped (%d): numHits = %d, numMisses = %d, %d%% miss rate, err = %v", i, numHits, numMisses, 100*numMisses/n, err)
+		assert.Nil(t, err, "One down - no errors")
 		assert.True(t, numHits < n, "One down - not all hit")
 		assert.True(t, numHits > 0, "One down - some hit")
 		assert.True(t, numMisses < n, "One down - not all missed")
@@ -111,7 +104,70 @@ func TestReverseProxyHash(t *testing.T) {
 	assert.Equal(t, n, accMisses, "Misses add up")
 }
 
-func measHitMiss(t *testing.T, client *redis.Client, n int, keyTempl string) (numHits, numMisses int, err error) {
+func makeServerList() (full redis.ServerList, broken redis.ServerList, onedowns []redis.ServerList) {
+	full = redis.ServerList{
+		{Name: "backend", Addr: "localhost:6379"},
+		{Name: "backend", Addr: "localhost:6380"},
+		{Name: "backend", Addr: "localhost:6381"},
+		{Name: "backend", Addr: "localhost:6382"},
+	}
+
+	broken = append(full, redis.ServerEndpoint{Name: "backend", Addr: "localhost:0"})
+
+	onedowns = []redis.ServerList{}
+	for i := 0; i < len(full); i++ {
+		// list containing all but the i'th element of full
+		notith := make(redis.ServerList, 0, len(full)-1)
+		for j := 0; j < len(full); j++ {
+			if j != i {
+				notith = append(notith, full[j])
+			}
+		}
+		onedowns = append(onedowns, notith)
+	}
+	return
+}
+
+type MultiError []error
+
+func (err MultiError) Error() string {
+	if len(err) > 0 {
+		return fmt.Sprintf("%d errors. First one: '%s'.", len(err), err[0])
+	} else {
+		return fmt.Sprintf("No errors (weird).")
+	}
+}
+
+func writeBlock(client *redis.Client, n int, keyTempl string) (numSuccess int, numFailure int, err MultiError) {
+	writeErrs := make(chan error, n)
+	waiter := &sync.WaitGroup{}
+	for i := 0; i < n; i++ {
+		key := fmt.Sprintf(keyTempl, i)
+		val := "1"
+		waiter.Add(1)
+		go func(key, val string) {
+			defer waiter.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+			if err := client.Exec(ctx, "SET", key, val); err != nil {
+				writeErrs <- err
+			}
+		}(key, val)
+	}
+	waiter.Wait()
+	close(writeErrs)
+	numFailure = len(writeErrs)
+	numSuccess = n - numFailure
+	if len(writeErrs) > 0 {
+		err = make(MultiError, 0)
+		for writeErr := range writeErrs {
+			err = append(err, writeErr)
+		}
+	}
+	return
+}
+
+func measHitMiss(client *redis.Client, n int, keyTempl string, timeout time.Duration) (numHits, numMisses int, err error) {
 	type tresult struct {
 		hit bool
 		err error
@@ -123,10 +179,13 @@ func measHitMiss(t *testing.T, client *redis.Client, n int, keyTempl string) (nu
 		waiter.Add(1)
 		go func(key string, rq chan<- tresult) {
 			defer waiter.Done()
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
 			defer cancel()
 			args := client.Query(ctx, "GET", key)
-			assert.Equal(t, 1, args.Len(), "GET returns a single value")
+			if args.Len() != 1 {
+				rq <- tresult{err: fmt.Errorf("Unexpected response: 1 arg expected; contains %d. ", args.Len())}
+				return
+			}
 			var v string
 			args.Next(&v)
 			if err := args.Close(); err != nil {
@@ -147,28 +206,6 @@ func measHitMiss(t *testing.T, client *redis.Client, n int, keyTempl string) (nu
 		} else {
 			numMisses++
 		}
-	}
-	return
-}
-
-func makeServerList() (full redis.ServerList, onedowns []redis.ServerList) {
-	full = redis.ServerList{
-		{Name: "backend", Addr: "localhost:6379"},
-		{Name: "backend", Addr: "localhost:6380"},
-		{Name: "backend", Addr: "localhost:6381"},
-		{Name: "backend", Addr: "localhost:6382"},
-	}
-
-	onedowns = []redis.ServerList{}
-	for i := 0; i < len(full); i++ {
-		// list containing all but the i'th element of full
-		notith := make(redis.ServerList, 0, len(full)-1)
-		for j := 0; j < len(full); j++ {
-			if j != i {
-				notith = append(notith, full[j])
-			}
-		}
-		onedowns = append(onedowns, notith)
 	}
 	return
 }
